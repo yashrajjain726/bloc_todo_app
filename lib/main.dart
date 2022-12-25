acb0db840af29da681ae2320a5197eb965cf10b2
Yashraj Jain <yashrajjain726@gmail.com>
2022-12-25 21:59:18 +0530

    injecting bloc in app done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
+import 'package:bloc_todo_app/cubits/active_todo_count/active_todo_count_cubit.dart';
+import 'package:bloc_todo_app/cubits/filter_todos/filter_todos_cubit.dart';
+import 'package:bloc_todo_app/cubits/todo_filter/todo_filter_cubit.dart';
+import 'package:bloc_todo_app/cubits/todo_list/todo_list_cubit.dart';
+import 'package:bloc_todo_app/cubits/todo_search/todo_search_cubit.dart';
 import 'package:bloc_todo_app/pages/todo_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +16,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Todo App',
-      debugShowCheckedModeBanner: false,
-      home: TodoPage(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<TodoFilterCubit>(create: (context) => TodoFilterCubit()),
+        BlocProvider<TodoSearchCubit>(create: (context) => TodoSearchCubit()),
+        BlocProvider<TodoListCubit>(create: (context) => TodoListCubit()),
+        BlocProvider<ActiveTodoCountCubit>(
+            create: (context) => ActiveTodoCountCubit(
+                todoListCubit: BlocProvider.of<TodoListCubit>(context))),
+        BlocProvider<FilterTodosCubit>(
+            create: (context) => FilterTodosCubit(
+                todoFilterCubit: BlocProvider.of<TodoFilterCubit>(context),
+                todoSearchCubit: BlocProvider.of<TodoSearchCubit>(context),
+                todoListCubit: BlocProvider.of<TodoListCubit>(context))),
+      ],
+      child: MaterialApp(
+        title: 'Todo App',
+        debugShowCheckedModeBanner: false,
+        home: TodoPage(),
+      ),
     );
   }
 }
