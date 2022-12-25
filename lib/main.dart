45fc11c0d359f76f31d5d8e7045f73a770956d3d
Yashraj Jain <yashrajjain726@gmail.com>
2022-12-25 19:05:52 +0530

    todo app model created

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bloc_todo_app/pages/todo_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      debugShowCheckedModeBanner: false,
+      home: TodoPage(),
+    );
+  }
+}
---
+++ lib/pages/todo_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TodoPage extends StatelessWidget {
+  const TodoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'TODOS',
+        ),
+      ),
+    );
+  }
+}
